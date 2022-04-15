bb549be9b0ace17cfa022600db5bd46162d748b1
César Acual <augustoacual@gmail.com>
2022-04-15 16:29:54 -0600

    Probar que un status no se pueda crear si el campo se encuentra vacío y que tenga al menos 5 caracteres

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -27,7 +27,7 @@ class CreateStatusTest extends TestCase
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
-        $response = $this->post(route('statuses.store'), ['body' => 'Mi primer status']);
+        $response = $this->postJson(route('statuses.store'), ['body' => 'Mi primer status']);
 
         $response->assertJson([
             'body' => 'Mi primer status',
@@ -36,6 +36,36 @@ class CreateStatusTest extends TestCase
         $this->assertDatabaseHas('statuses', [
             'user_id' => $user->id,
             'body' => 'Mi primer status'
+        ]);
+    }
+
+    /** @test */
+    function a_status_requires_a_body()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => '']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+
+    /** @test */
+    function a_status_body_requires_a_minimum_length()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => 'asdf']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
         ]);
     }
 }
