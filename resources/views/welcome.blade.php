f47b374ef9a097db61358c578a1666f1eeff3bfa
César Acual <augustoacual@gmail.com>
2022-11-16 17:59:09 -0600

    Diseniando el formulario de login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
-<form action="{{ route('statuses.store') }}" method="POST">
-    @csrf
-    <textarea name="body" cols="30" rows="10"></textarea>
-    <button id="create-status">Publicar estado</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-8 mx-auto">
+                <div class="card border-0 bg-light">
+                    <form action="{{ route('statuses.store') }}" method="POST">
+                        @csrf
+                        <div class="card-body">
+                            <textarea name="body" class="form-control border-0 bg-light" placeholder="¿Qué estás pensando Cesar?"></textarea>
+                        </div>
+                        <div class="card-footer">
+                            <button class="btn btn-primary" id="create-status">Publicar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
