54b5df0b2515d498e91c9929605ece3f8e7e4640
Augusto Acual <augustoacual@gmail.com>
2023-01-09 10:13:23 -0600

    Implementando Likes

--- tests/Unit/Models/StatusTest.php
+++ tests/Unit/Models/StatusTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Status;
+use App\Models\Like;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -18,5 +19,43 @@ class StatusTest extends TestCase
         $status = factory(Status::class)->create();
 
         $this->assertInstanceOf(User::class, $status->user);
+    }
+
+    /** @test */
+    function a_status_has_many_likes()
+    {
+        $status = factory(Status::class)->create();
+
+        factory(Like::class)->create(['status_id' => $status->id]);
+
+        $this->assertInstanceOf(Like::class, $status->likes->first());
+    }
+
+    /** @test */
+    function a_status_can_be_liked()
+    {
+        $status = factory(Status::class)->create();
+
+        $this->actingAs(factory(User::class)->create());
+
+        $status->like();
+
+        $this->assertEquals(1, $status->likes->count());
+    }
+
+    /** @test */
+    function a_status_can_be_liked_once()
+    {
+        $status = factory(Status::class)->create();
+
+        $this->actingAs(factory(User::class)->create());
+
+        $status->like();
+
+        $this->assertEquals(1, $status->likes->count());
+
+        $status->like();
+
+        $this->assertEquals(1, $status->fresh()->likes->count());
     }
 }
