aad9e07f667dc3014658ad778f25d00a30b3150e
Augusto Acual <augustoacual@gmail.com>
2023-01-20 18:41:04 -0600

    Mostrar el conteo de likes

--- public/js/app.js
+++ public/js/app.js
@@ -49876,7 +49876,7 @@ exports = module.exports = __webpack_require__(11)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -49887,6 +49887,7 @@ exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
 //
 //
 //
@@ -49950,11 +49951,13 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
         like: function like(status) {
             axios.post('/statuses/' + status.id + '/likes').then(function (res) {
                 status.is_liked = true;
+                status.likes_count++;
             });
         },
         unlike: function unlike(status) {
             axios.delete('/statuses/' + status.id + '/likes').then(function (res) {
                 status.is_liked = false;
+                status.likes_count--;
             });
         }
     }
@@ -50033,7 +50036,11 @@ var render = function() {
                   _c("i", { staticClass: "fa-regular fa-thumbs-up" }),
                   _vm._v("\n                ME GUSTA\n            ")
                 ]
-              )
+              ),
+          _vm._v(" "),
+          _c("span", { attrs: { dusk: "likes-count" } }, [
+            _vm._v(_vm._s(status.likes_count))
+          ])
         ])
       ])
     }),
