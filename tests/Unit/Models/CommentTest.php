da33c2b461996119ab46d1de6cb910fd404bc101
Augusto Acual <augustoacual@gmail.com>
2023-02-15 09:56:43 -0600

    Likes polimorficos - Parte 1

--- tests/Unit/Models/CommentTest.php
+++ tests/Unit/Models/CommentTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Comment;
+use App\Models\Like;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,12 +12,25 @@ use Tests\TestCase;
 class CommentTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     /** @test */
     function a_comment_belongs_to_a_user()
     {
         $comment = factory(Comment::class)->create();
 
         $this->assertInstanceOf(User::class, $comment->user);
+    }
+
+    /** @test */
+    function a_comment_morph_many_likes()
+    {
+        $comment = factory(Comment::class)->create();
+
+        factory(Like::class)->create([
+            'likeable_id' => $comment->id,
+            'likeable_type' => get_class($comment),
+        ]);
+
+        $this->assertInstanceOf(Like::class, $comment->likes->first());
     }
 }
